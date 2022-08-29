d703b0470a6f284b57b9051df9b6737cf7307ae9
aarvin-bstock <92053637+aarvin-bstock@users.noreply.github.com>
2022-08-29 18:22:40 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "test"
+
+@app.route("/hello")
+def index():
+    flash("what's your name bro?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi "+ str(request.form['name_input']) + ", great to see you")
+    request.form['name_input']
+    return render_template("index.html")
+
